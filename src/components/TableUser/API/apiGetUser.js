7ba29c28eea94ad228b5158f1dba35afce5c87b5
Anderson Esteban Rodriguez <114366197+3steban96@users.noreply.github.com>
2025-02-28 16:55:46 -0500

    Update apiGetUser.js

--- src/components/TableUser/API/apiGetUser.js
+++ src/components/TableUser/API/apiGetUser.js
@@ -3,7 +3,7 @@ import config from '../../../../config'
 
 export default async function apiGetUser() {
     try {
-        const response = await axios.get(`${config.API_URL}/getUser/`);
+        const response = await axios.get(`${config.API_URL}/getUser`);
         const data=response.data
         return data;
     } catch (error) {
