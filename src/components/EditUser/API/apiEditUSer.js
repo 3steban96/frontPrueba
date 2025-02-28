ea448bf3282b1855f0591941ed21a8f2d8328b80
Anderson Esteban Rodriguez <114366197+3steban96@users.noreply.github.com>
2025-02-28 16:54:12 -0500

    Update apiEditUSer.js

--- src/components/EditUser/API/apiEditUSer.js
+++ src/components/EditUser/API/apiEditUSer.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import config from '../../../../config'
 
-export default async function apiEditUser(dataUser, id) {
+export default async function apiEditUser(updatedUser) {
+    const id= updatedUser.idUser
     try {
-        const response = await axios.patch(`${config.API_URL}/editUser/${id}`, dataUser);
+        const response = await axios.patch(`${config.API_URL}/editUser/${id}`, updatedUser);
         return response.data;
     } catch (error) {
         console.error('Error al agregar usuario:', error);
